689a1b6b549a9c34b2afb92eb74bfc268e254ca1
Gino804 <giacomo.p03@gmail.com>
2023-07-18 17:34:46 +0200

    Added vote filter

--- index.php
+++ index.php
@@ -48,16 +48,25 @@ $hotels = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hotel</title>
+    <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <div class="container">
-        <form class="mt-5 d-flex align-items-center" action="">
-            <label for="parking-filter" class="form-check-label pe-2">Parcheggi necessari:</label>
-            <input type="checkbox" class="form-check-input" id="parking-filter" name="parking-filter">
+        <form class="mt-5" action="">
+            <label for="parking-filter" class="form-check-label">Parcheggi necessari:</label>
+            <input type="checkbox" class="form-check-input" id="parking-filter" name="parking-filter"><br>
 
-            <button type="submit" class="btn btn-primary ms-3">Filtra</button>
+            <div class="d-flex align-items-center my-2">
+                <label for="vote-filter">Valutazione minima: </label>
+                <input type="number" step="1" class="form-control ms-2" id="vote-filter" name="vote-filter">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Filtra</button>
         </form>
 
         <table class="table mt-5">
@@ -72,12 +81,16 @@ $hotels = [
             </thead>
             <tbody>
                 <?php foreach ($hotels as $hotel) : ?>
-                    <?php if (isset($_GET['parking-filter'])) : ?>
-                        <?php if ($hotel['parking']) : ?>
+                    <?php if (isset($_GET['parking-filter']) || $_GET['vote-filter']) : ?>
+                        <?php if ((isset($_GET['parking-filter']) && $hotel['parking']) || ($_GET['vote-filter'] && $hotel['vote'] >= $_GET['vote-filter'])) : ?>
                             <tr>
                                 <?php foreach ($hotel as $key => $value) : ?>
                                     <td>
-                                        <?= $key == 'parking' ? 'Sì' : $value ?>
+                                        <?php
+                                        if ($key == 'parking') {
+                                            echo $value ? 'Sì' : 'No';
+                                        } else echo $value;
+                                        ?>
                                     </td>
                                 <?php endforeach ?>
                             </tr>
